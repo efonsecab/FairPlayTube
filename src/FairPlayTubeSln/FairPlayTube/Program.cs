85cec819be020ff3f1ea90c699140c2ec12b7694
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2021-05-15 11:08:55 -0600

    Fixing projects and folders names

--- src/FairPlayTubeSln/FairPlayTube/Program.cs
+++ src/FairPlayTubeSln/FairPlayTube/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PlayFairTube
+namespace FairPlayTube
 {
     public class Program
     {
--- src/FairPlayTubeSln/FairPlayTube/Startup.cs
+++ src/FairPlayTubeSln/FairPlayTube/Startup.cs
@@ -12,7 +12,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PlayFairTube
+namespace FairPlayTube
 {
     public class Startup
     {
