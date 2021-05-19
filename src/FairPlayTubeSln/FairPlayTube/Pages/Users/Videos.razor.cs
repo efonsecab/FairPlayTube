c54a904ceb13731723432e368c629941ce32b7e8
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2021-05-19 07:22:29 -0600

    Adding draft for displaying public videos

---
+++ src/FairPlayTubeSln/FairPlayTube/Pages/Users/Videos.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Pages.Users
+{
+    [Route("/Users/Videos")]
+    [Authorize(Roles = Common.Global.Constants.Roles.User)]
+    public partial class Videos
+    {
+    }
+}
--- src/FairPlayTubeSln/FairPlayTube/Startup.cs
+++ src/FairPlayTubeSln/FairPlayTube/Startup.cs
@@ -1,18 +1,29 @@
+using FairPlayTube.Common.Interfaces;
+using FairPlayTube.CustomProviders;
 using FairPlayTube.DataAccess.Data;
+using FairPlayTube.DataAccess.Models;
+using FairPlayTube.Models;
+using FairPlayTube.Services;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using PTI.Microservices.Library.Configuration;
+using PTI.Microservices.Library.Interceptors;
+using PTI.Microservices.Library.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace FairPlayTube
@@ -30,24 +41,93 @@ namespace FairPlayTube
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
+            GlobalPackageConfiguration.RapidApiKey = Configuration.GetValue<string>("RapidApiKey");
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<ICurrentUserProvider, CurrentUserProvider>();
+            services.AddScoped(serviceProvider =>
+            {
+                var fairplaytubeDatabaseContext = this.CreateFairPlayTubeDbContext(services);
+                return fairplaytubeDatabaseContext;
+            });
+
+
+            services.AddTransient<CustomHttpClientHandler>();
+            services.AddTransient<CustomHttpClient>();
+
+            ConfigureAzureVideoIndexer(services);
+
+            services.AddTransient<VideoService>();
+
+            services.AddTransient<ToastifyService>();
 
             services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAdB2C"));
 
-            services.AddControllersWithViews()
-                .AddMicrosoftIdentityUI();
+            services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.TokenValidationParameters.NameClaimType = "name";
+                options.TokenValidationParameters.RoleClaimType = "Role";
+                options.Events.OnTokenValidated = async (context) =>
+                {
+                    FairplaytubeDatabaseContext fairplaytubeDatabaseContext = CreateFairPlayTubeDbContext(services);
+                    ClaimsIdentity claimsIdentity = context.Principal.Identity as ClaimsIdentity;
+                    var userObjectIdClaim = claimsIdentity.Claims.Single(p => p.Type == Common.Global.Constants.Claims.ObjectIdentifier);
+                    var user = await fairplaytubeDatabaseContext.ApplicationUser
+                    .Include(p => p.ApplicationUserRole)
+                    .ThenInclude(p => p.ApplicationRole)
+                    .Where(p => p.AzureAdB2cobjectId.ToString() == userObjectIdClaim.Value)
+                    .SingleOrDefaultAsync();
+                    if (user != null && user.ApplicationUserRole != null)
+                    {
+                        claimsIdentity.AddClaim(new Claim("Role", user.ApplicationUserRole.ApplicationRole.Name));
+                    }
+                    else
+                    {
+                        if (user == null)
+                        {
+                            var userRole = await fairplaytubeDatabaseContext.ApplicationRole.FirstAsync(p => p.Name == Common.Global.Constants.Roles.User);
+                            user = new ApplicationUser()
+                            {
+                                LastLogIn = DateTimeOffset.UtcNow,
+                                FullName = "Test1",
+                                EmailAddress = "Test2",
+                                AzureAdB2cobjectId = Guid.Parse(userObjectIdClaim.Value)
+                            };
+                            await fairplaytubeDatabaseContext.ApplicationUser.AddAsync(user);
+                            await fairplaytubeDatabaseContext.SaveChangesAsync();
+                            await fairplaytubeDatabaseContext.ApplicationUserRole.AddAsync(new ApplicationUserRole()
+                            {
+                                ApplicationUserId = user.ApplicationUserId,
+                                ApplicationRoleId = userRole.ApplicationRoleId
+                            });
+                            await fairplaytubeDatabaseContext.SaveChangesAsync();
+                            claimsIdentity.AddClaim(new Claim("Role", user.ApplicationUserRole.ApplicationRole.Name));
+                        }
+                    }
+                };
+            });
+
+            services.AddControllersWithViews().AddMicrosoftIdentityUI();
 
             services.AddAuthorization(options =>
             {
                 // By default, all incoming requests will be authorized according to the default policy
-                options.FallbackPolicy = options.DefaultPolicy;
+                //options.FallbackPolicy = options.DefaultPolicy;
             });
 
             services.AddServerSideBlazor()
                 .AddMicrosoftIdentityConsentHandler();
+            services.AddRazorPages();
 
+        }
+
+        private void ConfigureAzureVideoIndexer(IServiceCollection services)
+        {
+            AzureVideoIndexerConfiguration azureVideoIndexerConfiguration =
+                Configuration.GetSection($"AzureConfiguration:{nameof(AzureVideoIndexerConfiguration)}")
+                .Get<AzureVideoIndexerConfiguration>();
+            services.AddSingleton(azureVideoIndexerConfiguration);
+            services.AddTransient<AzureVideoIndexerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -77,8 +157,8 @@ namespace FairPlayTube
                         try
                         {
                             FairplaytubeDatabaseContext fairplaytubeDatabaseContext =
-                            this.CreateDbContext(context.RequestServices);
-                            await fairplaytubeDatabaseContext.ErrorLog.AddAsync(new FairPlayTube.DataAccess.Models.ErrorLog()
+                            this.CreateFairPlayTubeDbContext(context.RequestServices);
+                            await fairplaytubeDatabaseContext.ErrorLog.AddAsync(new ErrorLog()
                             {
                                 FullException = error.ToString(),
                                 StackTrace = error.StackTrace,
@@ -88,8 +168,13 @@ namespace FairPlayTube
                         }
                         catch (Exception)
                         {
-
+                            throw;
                         }
+                        ProblemHttpResponse problemHttpResponse = new()
+                        {
+                            Detail = error.Message,
+                        };
+                        await context.Response.WriteAsJsonAsync<ProblemHttpResponse>(problemHttpResponse);
                     }
                 });
             });
@@ -99,6 +184,9 @@ namespace FairPlayTube
 
             app.UseRouting();
 
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapBlazorHub();
@@ -107,9 +195,27 @@ namespace FairPlayTube
             });
         }
 
-        private FairplaytubeDatabaseContext CreateDbContext(IServiceProvider requestServices)
+        private FairplaytubeDatabaseContext CreateFairPlayTubeDbContext(IServiceCollection services)
         {
-            throw new NotImplementedException();
+            var sp = services.BuildServiceProvider();
+            DbContextOptionsBuilder<FairplaytubeDatabaseContext> dbContextOptionsBuilder =
+                new();
+            FairplaytubeDatabaseContext fairplaytubeDatabaseContext =
+            new(dbContextOptionsBuilder.UseSqlServer(Configuration.GetConnectionString("Default"),
+            sqlServerOptionsAction: (serverOptions) => serverOptions.EnableRetryOnFailure(3)).Options,
+            sp.GetService<ICurrentUserProvider>());
+            return fairplaytubeDatabaseContext;
+        }
+
+        private FairplaytubeDatabaseContext CreateFairPlayTubeDbContext(IServiceProvider serviceProvider)
+        {
+            DbContextOptionsBuilder<FairplaytubeDatabaseContext> dbContextOptionsBuilder =
+                            new();
+            FairplaytubeDatabaseContext fairplaytubeDatabaseContext =
+            new(dbContextOptionsBuilder.UseSqlServer(Configuration.GetConnectionString("Default"),
+            sqlServerOptionsAction: (serverOptions) => serverOptions.EnableRetryOnFailure(3)).Options,
+            serviceProvider.GetService<ICurrentUserProvider>());
+            return fairplaytubeDatabaseContext;
         }
     }
 }
