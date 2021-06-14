a4d507b829489bd0d46c2e87082cb18e0519762c
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2021-06-14 13:56:35 -0600

    Adding EmailService

---
+++ src/FairPlayTubeSln/FairPlayTube.Models/Invites/InviteUserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Models.Invites
+{
+    public class InviteUserModel
+    {
+        [Required]
+        [EmailAddress]
+        public string ToEmailAddress { get; set; }
+        [Required]
+        public string CustomMessage { get; set; }
+    }
+}
---
+++ src/FairPlayTubeSln/FairPlayTube.Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using FairPlayTube.Services.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Services
+{
+    public class EmailService
+    {
+        private SmtpConfiguration SmtpConfiguration { get; }
+        public EmailService(SmtpConfiguration smtpconfiguration)
+        {
+            this.SmtpConfiguration = smtpconfiguration;
+        }
+
+        public async Task SendEmail(string toEmailAddress, string subject, string body,
+            bool isBodyHtml)
+        {
+            MailMessage msg = new MailMessage();
+            msg.To.Add(new MailAddress(toEmailAddress));
+            msg.From = new MailAddress(this.SmtpConfiguration.SenderEmail, this.SmtpConfiguration.SenderDisplayName);
+            msg.Subject = subject;
+            msg.Body = body;
+            msg.IsBodyHtml = isBodyHtml;
+
+            SmtpClient client = new SmtpClient();
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential(this.SmtpConfiguration.SenderUsername,
+                this.SmtpConfiguration.SenderPassword);
+            client.Port = this.SmtpConfiguration.Port;
+            client.Host = this.SmtpConfiguration.Server;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.EnableSsl = true;
+            await client.SendMailAsync(msg).ConfigureAwait(continueOnCapturedContext: true);
+        }
+    }
+}
