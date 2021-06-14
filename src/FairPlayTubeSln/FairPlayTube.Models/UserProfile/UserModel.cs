46db07006c7930e84847d84a87f1ad43ccc766ab
Eduardo Fonseca <eduardo.fonseca@pticostarica.com>
2021-06-14 11:11:25 -0600

    Adding endpoint to list users

---
+++ src/FairPlayTubeSln/FairPlayTube.Models/UserProfile/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Models.UserProfile
+{
+    public class UserModel
+    {
+        public long ApplicationUserId { get; set; }
+        public string Name { get; set; }
+        public long BrandsCount { get; set; }
+        public long VideosCount { get; set; }
+    }
+}
