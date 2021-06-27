e4752dd393f5a0d67a505d31ff976430c3a5c4d5
Federico José Lucia <federico.lucia@pticostarica.com>
2021-06-27 22:57:03 +0000

    Send Message Endpoint created

---
+++ src/FairPlayTubeSln/FairPlayTube.Models/UserMessage/UserMessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Models.UserMessage
+{
+    public class UserMessageModel
+    {
+        public long ToApplicationUserId { get; set; }
+        public string Message { get; set; }
+    }
+}
--- src/FairPlayTubeSln/FairPlayTube/Controllers/UserController.cs
+++ src/FairPlayTubeSln/FairPlayTube/Controllers/UserController.cs
@@ -1,18 +1,21 @@
 ﻿using FairPlayTube.Common.Interfaces;
 using FairPlayTube.DataAccess.Data;
 using FairPlayTube.Models.Invites;
+using FairPlayTube.Models.UserMessage;
 using FairPlayTube.Models.UserProfile;
 using FairPlayTube.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using FairPlayTube.Notifications.Hubs;
 
 namespace FairPlayTube.Controllers
 {
@@ -24,13 +27,16 @@ namespace FairPlayTube.Controllers
         private FairplaytubeDatabaseContext FairplaytubeDatabaseContext { get; }
         private ICurrentUserProvider CurrentUserProvider { get; }
         private EmailService EmailService { get; }
+        private IHubContext<NotificationHub, INotificationHub> HubContext { get; }
 
         public UserController(FairplaytubeDatabaseContext fairplaytubeDatabaseContext, 
-            ICurrentUserProvider currentUserProvider, EmailService emailService)
+            ICurrentUserProvider currentUserProvider, EmailService emailService, 
+            IHubContext<NotificationHub, INotificationHub> hubContext)
         {
             this.FairplaytubeDatabaseContext = fairplaytubeDatabaseContext;
             this.CurrentUserProvider = currentUserProvider;
             this.EmailService = emailService;
+            this.HubContext = hubContext;
         }
 
         /// <summary>
@@ -77,6 +83,35 @@ namespace FairPlayTube.Controllers
             await this.EmailService.SendEmail(model.ToEmailAddress, $"{userName} is inviting you to " +
                 $"FairPlayTube: The Next Generation Of Video Sharing Portals",
                 completeBody.ToString(), true);
+        }
+
+        [HttpPost("[action]")]
+        public async Task SendMessage(UserMessageModel model, CancellationToken cancellationToken)
+        {
+            var senderObjectId = this.CurrentUserProvider.GetObjectId();
+            var sender = await this.FairplaytubeDatabaseContext.ApplicationUser
+                .Where(u => u.AzureAdB2cobjectId.ToString() == senderObjectId)
+                .SingleAsync(cancellationToken);
+
+            var receiver = await this.FairplaytubeDatabaseContext.ApplicationUser
+                .Where(u => u.ApplicationUserId == model.ToApplicationUserId)
+                .SingleAsync(cancellationToken);
+
+            await this.FairplaytubeDatabaseContext.UserMessage.AddAsync(
+                new DataAccess.Models.UserMessage()
+                {
+                    ToApplicationUserId = model.ToApplicationUserId,
+                    Message = model.Message,
+                    FromApplicationUserId = sender.ApplicationUserId
+                }, cancellationToken);
+
+            await this.FairplaytubeDatabaseContext.SaveChangesAsync();
+
+            await this.HubContext.Clients.User(receiver.AzureAdB2cobjectId.ToString())
+                .ReceiveMessage(new Models.Notifications.NotificationModel()
+                {
+                    Message = $"You have a new message from: {sender.FullName}"
+                });
         }
     }
 }
