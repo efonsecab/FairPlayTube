fb62ef9626d3c8ab7af3a6ce4fa382533a54e89d
Eduardo Fonseca <efonsecab@gmail.com>
2021-07-25 17:00:03 -0600

    Adding page to check pending videos status

---
+++ src/FairPlayTubeSln/FairPlayTube.Client/Pages/Users/Videos/MyPendingVideosStatus.razor.cs
@@ -0,0 +1,41 @@
+﻿using FairPlayTube.Client.Services;
+using FairPlayTube.ClientServices;
+using FairPlayTube.Models.Video;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Client.Pages.Users.Videos
+{
+    [Route(Common.Global.Constants.UserPagesRoutes.MyPendingVideosStatus)]
+    [Authorize(Roles = Common.Global.Constants.Roles.User)]
+    public partial class MyPendingVideosStatus
+    {
+        [Inject]
+        private VideoClientService VideoClientService { get; set; }
+        [Inject]
+        private ToastifyService ToastifyService { get; set; }
+        private bool IsLoading { get; set; }
+        private List<VideoStatusModel> MyPendingVideosQueue { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                IsLoading = true;
+                this.MyPendingVideosQueue = await VideoClientService.GetMyPendingVideosQueue();
+            }
+            catch (Exception ex)
+            {
+                await this.ToastifyService.DisplayErrorNotification(ex.Message);
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+    }
+}
