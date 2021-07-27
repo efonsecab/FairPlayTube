51fdf92a63260b671537a9a0af1e69d8d8ef85c4
Eduardo Fonseca <efonsecab@gmail.com>
2021-07-27 06:55:56 -0600

    Adding PricingHelper

---
+++ src/FairPlayTubeSln/FairPlayTube.Common/Helpers/PricingHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Common.Helpers
+{
+    public static class PricingHelper
+    {
+        public static decimal CalculateVideoAccessTotalPrice(decimal videoPrice)
+        {
+            var defaultCommision = Global.Constants.Commissions.VideoAccess;
+            var totalPrice = videoPrice + (videoPrice * defaultCommision);
+            return totalPrice;
+        }
+    }
+}
