c940fd00cb43d9da464d0bcc17ff61a07433177b
Eduardo Fonseca <efonsecab@gmail.com>
2021-07-31 10:21:42 -0600

    Adding VideoCommentAnalysis table

---
+++ src/FairPlayTubeSln/FairPlayTube.Database/dbo/Tables/VideoCommentAnalysis.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[VideoCommentAnalysis]
+(
+	[VideoCommentAnalysis] BIGINT NOT NULL CONSTRAINT PK_VideoCommentAnalysis PRIMARY KEY IDENTITY, 
+    [VideoCommentId] BIGINT NOT NULL, 
+    [Sentiment] FLOAT NOT NULL, 
+    [KeyPhrases] TEXT NOT NULL, 
+    [Topics] TEXT NOT NULL, 
+    CONSTRAINT [FK_VideoCommentAnalysis_VideoComment] FOREIGN KEY ([VideoCommentId]) REFERENCES [VideoComment]([VideoCommentId]) 
+)
+
+GO
+
+CREATE UNIQUE INDEX [UI_VideoCommentAnalysis_VideoCommentId] ON [dbo].[VideoCommentAnalysis] ([VideoCommentId])
