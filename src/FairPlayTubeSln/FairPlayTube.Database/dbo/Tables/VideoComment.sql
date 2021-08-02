a9aa7790049358fa31f80191f5f48091b22d859c
luisiitoDev <luisasandovalcode@gmail.com>
2021-08-02 10:29:12 -0600

    it was added the IOriginalInfo columns for the VideoComment Table

--- src/FairPlayTubeSln/FairPlayTube.Database/dbo/Tables/VideoComment.sql
+++ src/FairPlayTubeSln/FairPlayTube.Database/dbo/Tables/VideoComment.sql
@@ -3,5 +3,9 @@
 	[VideoCommentId] BIGINT NOT NULL CONSTRAINT PK_VideoComment PRIMARY KEY,
 	[VideoInfoId] BIGINT NOT NULL CONSTRAINT FK_VideoInfo_VideoComment REFERENCES [dbo].[VideoInfo]([VideoInfoId]),
 	[ApplicationUserId] BIGINT NOT NULL CONSTRAINT FK_ApplicationUserId_VideoComment REFERENCES [dbo].[ApplicationUser]([ApplicationUserId]),
-	[Comment] NVARCHAR(500) NOT NULL
+	[Comment] NVARCHAR(500) NOT NULL,
+	[RowCreationDateTime] DATETIMEOFFSET NOT NULL, 
+    [RowCreationUser] NVARCHAR(256),
+    [SourceApplication] NVARCHAR(250), 
+    [OriginatorIPAddress] NVARCHAR(100),
 )
