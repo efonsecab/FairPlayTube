55e0a7ba48989415f858fac410e745478f536cab
Eduardo Fonseca <efonsecab@gmail.com>
2021-08-07 13:09:48 -0600

    Implementing the AddUserFollowerAsync method

---
+++ src/FairPlayTubeSln/FairPlayTube.Models/UserAudience/UserFollowerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Models.UserAudience
+{
+    /// <summary>
+    /// Represents a follower for a given user
+    /// </summary>
+    public class UserFollowerModel
+    {
+        /// <summary>
+        /// Id for the user following
+        /// </summary>
+        public long FollowedApplicationUserId { get; set; }
+        /// <summary>
+        /// Id for the user being followed
+        /// </summary>
+        public long FollowerApplicationUserId { get; set; }
+    }
+}
---
+++ src/FairPlayTubeSln/FairPlayTube.Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using FairPlayTube.DataAccess.Data;
+using FairPlayTube.Models.UserAudience;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Services
+{
+    public class UserService
+    {
+        private FairplaytubeDatabaseContext FairplaytubeDatabaseContext { get; }
+        public UserService(FairplaytubeDatabaseContext fairplaytubeDatabaseContext)
+        {
+            this.FairplaytubeDatabaseContext = fairplaytubeDatabaseContext;
+        }
+
+        public async Task AddUserFollowerAsync(UserFollowerModel userFollowerModel, CancellationToken cancellationToken)
+        {
+            var followerUserEntity = await this.FairplaytubeDatabaseContext.ApplicationUser
+                .SingleOrDefaultAsync(p => p.ApplicationUserId == userFollowerModel.FollowerApplicationUserId);
+            if (followerUserEntity is null)
+                throw new Exception($"User {userFollowerModel.FollowerApplicationUserId} does not exist");
+
+            var followedUserEntity = await this.FairplaytubeDatabaseContext.ApplicationUser
+                .SingleOrDefaultAsync(p => p.ApplicationUserId == userFollowerModel.FollowedApplicationUserId);
+            if (followedUserEntity is null)
+                throw new Exception($"User {userFollowerModel.FollowedApplicationUserId} does not exist");
+
+            await this.FairplaytubeDatabaseContext.UserFollower.AddAsync(new DataAccess.Models.UserFollower() 
+            {
+                FollowerApplicationUserId = followerUserEntity.ApplicationUserId,
+                FollowedApplicationUserId = followedUserEntity.ApplicationUserId
+            });
+            await this.FairplaytubeDatabaseContext.SaveChangesAsync();
+        }
+
+    }
+}
