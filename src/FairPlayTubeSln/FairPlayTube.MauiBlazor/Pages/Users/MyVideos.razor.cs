b0311bfcfefbb6060cfedfdf2fe59d24b8520162
Eduardo Fonseca <efonsecab@gmail.com>
2021-11-09 06:38:28 -0600

    Adding draft login button to MAUI App

---
+++ src/FairPlayTubeSln/FairPlayTube.MauiBlazor/Pages/Users/MyVideos.razor.cs
@@ -0,0 +1,45 @@
+﻿using FairPlayTube.Client.Services;
+using FairPlayTube.ClientServices;
+using FairPlayTube.Models.Video;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.MauiBlazor.Pages.Users
+{
+    [Route(Common.Global.Constants.UserPagesRoutes.MyVideos)]
+    [Authorize(Roles = Common.Global.Constants.Roles.User)]
+    public partial class MyVideos
+    {
+        private VideoInfoModel[] AllVideos { get; set; }
+        [Inject]
+        private VideoClientService VideoClientService { get; set; }
+        [Inject]
+        private ToastifyService ToastifyService { get; set; }
+        private bool IsLoading { get; set; }
+        private VideoInfoModel SelectedVideo { get; set; }
+        private bool ShowModal { get; set; } = false;
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+        protected async override Task OnInitializedAsync()
+        {
+            try
+            {
+                IsLoading = true;
+                this.AllVideos = await this.VideoClientService.GetMyProcessedVideos();
+            }
+            catch (Exception ex)
+            {
+                await this.ToastifyService.DisplayErrorNotification(ex.Message);
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+        }
+    }
+}
