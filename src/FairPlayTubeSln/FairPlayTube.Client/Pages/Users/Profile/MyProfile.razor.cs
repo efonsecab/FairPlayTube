ad8f15112666e31b7fc81c31cea384f8e69e31a7
Eduardo Fonseca <efonsecab@gmail.com>
2021-11-15 18:27:14 -0600

    Adding draft for "MyProfile" page

---
+++ src/FairPlayTubeSln/FairPlayTube.Client/Pages/Users/Profile/MyProfile.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+
+namespace FairPlayTube.Client.Pages.Users.Profile
+{
+    [Route(Common.Global.Constants.UserPagesRoutes.MyProfile)]
+    [Authorize(Roles = Common.Global.Constants.Roles.User)]
+    public partial class MyProfile
+    {
+    }
+}
