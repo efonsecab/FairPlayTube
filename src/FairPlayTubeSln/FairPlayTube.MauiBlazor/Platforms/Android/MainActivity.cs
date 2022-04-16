98fe1d78127812ee60e7a400ebbdd45972f91548
Eduardo Fonseca <efonsecab@gmail.com>
2022-04-16 13:43:19 -0600

    Setting ParentWindows in the Android app

--- src/FairPlayTubeSln/FairPlayTube.MauiBlazor/Platforms/Android/MainActivity.cs
+++ src/FairPlayTubeSln/FairPlayTube.MauiBlazor/Platforms/Android/MainActivity.cs
@@ -7,5 +7,10 @@ namespace FairPlayTube.MauiBlazor
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            App.ParentWindow = this;
+            base.OnCreate(savedInstanceState);
+        }
     }
 }
