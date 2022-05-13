ea85d15934f70ba839cfb12b8f44a1c8f3284e8c
Eduardo Fonseca <efonsecab@gmail.com>
2022-05-13 17:50:02 -0600

    Base for CSS Grid Layout

---
+++ src/FairPlayTubeSln/FairPlayTube.Client/Shared/CssGridLayout.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace FairPlayTube.Client.Shared
+{
+    public partial class CssGridLayout
+    {
+        private bool ShowCultureSelector { get; set; }        
+        private void OnShowCultureSelectorClicked()
+        {
+            this.ShowCultureSelector = true;
+        }
+        private void HideCultureSelector()
+        {
+            this.ShowCultureSelector = false;
+        }
+    }
+}
