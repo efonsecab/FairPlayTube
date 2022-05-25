e7287b7fce1fd82c8723a45a4a0e61a3642d78cf
Luis Sandoval Monestel <79773016+LuisiitoDev@users.noreply.github.com>
2022-05-24 18:13:26 -0600

    it was created the role request model and the role quest service

---
+++ src/FairPlayTubeSln/FairPlayTube.Models/RoleRequest/AddRoleRequestModel.cs
@@ -0,0 +1,23 @@
+﻿namespace FairPlayTube.Models.RoleRequest
+{
+    /// <summary>
+    /// Holds the information for a Role Request
+    /// </summary>
+    public class AddRoleRequestModel
+    {
+        /// <summary>Role Request Name</summary>
+        public string Name { get; set; }
+        /// <summary>Role Request Last Name</summary>
+        public string Lastname { get; set; }
+        /// <summary>Role Request Last Surname</summary>
+        public string Surname { get; set; }
+        /// <summary>Role Request Nationality</summary>
+        public string Nationality { get; set; }
+        /// <summary>Role Request NationalId</summary>
+        public int NationalId { get; set; }
+        /// <summary>Role Request National Id Photo Stored File Name</summary>
+        public string NationalIdPhotoStoredFileName { get; set; }
+        /// <summary>Role Request Request Application Role</summary>
+        public int RequestedApplicationRole { get; set; }
+    }
+}
---
+++ src/FairPlayTubeSln/FairPlayTube.Services/RoleRequestClientService.cs
@@ -0,0 +1,22 @@
+﻿using FairPlayTube.DataAccess.Data;
+using FairPlayTube.Models.RoleRequest;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FairPlayTube.Services
+{
+    public class RoleRequestClientService
+    {
+        private readonly FairplaytubeDatabaseContext FairplaytubeDatabaseContext;
+        public RoleRequestClientService(FairplaytubeDatabaseContext FairplaytubeDatabaseContext)
+        {
+            this.FairplaytubeDatabaseContext = FairplaytubeDatabaseContext;
+        }
+
+        public Task AddRoleRequestAsync(AddRoleRequestModel addRoleRequestModel, CancellationToken cancellationToken)
+        {
+            //TODO: IT IS REQUIRED THE DB CONTEXT FOR ROLE REQUEST TABLE
+            return Task.CompletedTask;
+        }
+    }
+}
