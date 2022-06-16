552e1af690f20c4435f5538fd6255f10fa3850a8
Eduardo Fonseca <efonsecab@gmail.com>
2022-06-16 06:18:16 -0600

    Setting conditiional compilation for msal client id

--- src/FairPlayTubeSln/FairPlayTube.MauiBlazor/Platforms/Android/MsalActivity.cs
+++ src/FairPlayTubeSln/FairPlayTube.MauiBlazor/Platforms/Android/MsalActivity.cs
@@ -10,11 +10,19 @@ using System.Threading.Tasks;
 namespace FairPlayTube.MauiBlazor.Platforms.Android
 {
     [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop, Exported = true)]
+#if DEBUG
     [IntentFilter(new[] { global::Android.Content.Intent.ActionView },
         Categories = new[] { global::Android.Content.Intent.CategoryBrowsable,
             global::Android.Content.Intent.CategoryDefault },
         DataHost = "auth",
-        DataScheme = "msal3fe0e8b7-12c1-47e5-bd6a-63b47504c5ee")]
+        DataScheme = "msal110facd2-07c2-4ecd-80e4-aeb3627100cf")]
+#else
+    [IntentFilter(new[] { global::Android.Content.Intent.ActionView },
+            Categories = new[] { global::Android.Content.Intent.CategoryBrowsable,
+                global::Android.Content.Intent.CategoryDefault },
+            DataHost = "auth",
+            DataScheme = "msal3fe0e8b7-12c1-47e5-bd6a-63b47504c5ee")]
+#endif
     public class MsalActivity : BrowserTabActivity
     {
     }
